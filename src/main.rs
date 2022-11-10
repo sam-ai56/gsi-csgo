8802a68614b47cffeb3edca976e4f66f136696a6
Sam Miller <116034199+sam-ai56@users.noreply.github.com>
2022-11-10 18:52:42 +0200

    Poem server added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{handler, listener::TcpListener, post, Route, Server};
+
+#[handler]
+async fn payload(req: String) {
+    println!("{:#}", req)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    tracing_subscriber::fmt::init();
+
+    let app = Route::new().at("/", post(payload));
+    
+    Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .run(app)
+        .await
 }
